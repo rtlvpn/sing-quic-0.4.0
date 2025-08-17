e9155d925e7ddc37e632a1db92c6bda19abdb939
rtlvpn <iceking@rattleprotocol.store>
2025-08-17 12:28:57 -0600

    🧃

--- hysteria2/client.go
+++ hysteria2/client.go
@@ -66,7 +66,6 @@ type Client struct {
 	udpDisabled        bool
 
 	connAccess sync.Mutex
-	connErr    error
 	conn       *clientQUICConnection
 }
 
@@ -145,9 +144,6 @@ func parsePorts(serverPorts []string) ([]uint16, error) {
 func (c *Client) offer(ctx context.Context) (*clientQUICConnection, error) {
 	c.connAccess.Lock()
 	defer c.connAccess.Unlock()
-	if c.connErr != nil {
-		return nil, c.connErr
-	}
 	conn := c.conn
 	if conn != nil && conn.active() {
 		return conn, nil
@@ -292,14 +288,10 @@ func (c *Client) ListenPacket(ctx context.Context) (net.PacketConn, error) {
 func (c *Client) CloseWithError(err error) error {
 	c.connAccess.Lock()
 	defer c.connAccess.Unlock()
-	if c.connErr != nil {
-		return nil
-	}
 	conn := c.conn
 	if conn != nil {
 		conn.closeWithError(err)
 	}
-	c.connErr = err
 	return nil
 }
 
